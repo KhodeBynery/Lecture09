188236cc9fe5152d3b03fc05450e81db3c17c883
anonmape <anonmape@greenwood.com>
2018-12-20 22:30:30 -0400

    Lecture 9

--- app.js
+++ app.js
@@ -4,8 +4,25 @@
 angular.module('DIApp', [])
 .controller('DIController', DIController);
 
-function DIController ($scope) {
+function DIController ($scope,
+                       $filter,
+                       $injector) {
   $scope.name = "Khode";
+
+  $scope.upper = function () {
+    var upCase = $filter('uppercase');
+    $scope.name = upCase($scope.name);
+  };
+
+  console.log($injector.annotate(DIController))
+  ;
 }
+
+function AnnotateMe(name, job, blah) {
+  return "Blah!";
+}
+
+console.log(DIController.toString);
+// console.log(AnnotateMe());
 
 })();
